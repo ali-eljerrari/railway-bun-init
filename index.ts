efc6300ac51cfa401fed473014b889e6314c96fa
Coding Dynasty <contact@coding-dynasty.com>
2025-03-27 17:44:49 +0000

    feat: add story fetching from JSON data

--- index.ts
+++ index.ts
@@ -1,8 +1,32 @@
+import data from './csvjson.json'
+
+interface Story {
+  [key: string]: any;
+}
+
+const stories = data as Story[];
+
 const server = Bun.serve({
-  hostname: "::",
   port: process.env.PORT ?? 3000,
   fetch(request) {
-    return new Response("Welcome to Bun!");
+    const ip = request.headers.get('x-forwarded-for')
+    const url = request.url
+    const method = request.method
+    console.log(`Received request from ${ip} ${url} ${method}`)
+
+    const story = stories[Math.floor(Math.random() * stories.length)]
+    const index = stories.indexOf(story)
+    const total = stories.length
+    return new Response(JSON.stringify({
+      status: 200,
+      data: {
+        story,
+        index,
+        total,
+      },
+    }), {
+      headers: { 'Content-Type': 'application/json' }
+    })
   },
 });
 
